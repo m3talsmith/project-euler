2ee8769e84a39f2d1cad34bfe0ae9f30a0652505
Michael Christenson II <michael@deepwoodsbrigade.com>
2013-10-09 07:54:07 -0700

    back to basic fib

--- problem_2/sequence.erl
+++ problem_2/sequence.erl
@@ -1,29 +1,15 @@
 -module(sequence).
--export(main/0).
+-export([main/1,fib/1]).
 
-main() ->
-  io:format("Starting Fibonacci sequences~n"),
-  StartTime = time(),
-  MaxSum    = 4000000,
-  Sequence  = fibonacci_sum(MaxSum),
-  EndTime   = time(),
-  TimeSpent = EndTime - StartTime
-  io:format("Finished!~n"),
-  io:format("Sequence: ~s~n", [Sequence]),
-  io:format("Finished in ~s minutes~n", [TimeSpent]).
+main(MaxSum) ->
+  io:format("Starting Fibonacci sequences: ~p~n", [time()]),
+  Sequence  = fib(MaxSum),
+  io:format("Finished! ~p~n", [time()]),
+  io:format("Sequence: ~p~n", [Sequence]).
 
-fibonacci_sum(0) ->
-  0;
-fibonacci_sum(N) when N =:= 1 or N =:= 2 ->
-  1;
-fibonacci_sum(N) when N rem 2 =:= 0 ->
-  M = N / 2,
-  fast_fibonacci(M) * (2 * fast_fibonacci(M + 1) - fast_fibonacci(M));
-fibonacci_sum(N) when N rem 2 =:= 1 ->
-  L = (N - 1) / 2,
-  math:pow(fast_fibonacci(L + 1), 2) + math:pow(fast_fibonacci(L), 2).
-
-fibonacci_sum(N) >= MaxSum ->
-  N;
-fibonacci_sum(N) < MaxSum ->
-  fibonacci_sum(fast_fibonaci(N)).
+fib(N) ->
+  fib(N,0,1,[0]).
+fib(0, Current, Next, FibList) ->
+  lists:reverse(FibList);
+fib(N, Current, Next, FibList) ->
+  fib(N - 1, Next, Current + Next, [Next | FibList]).
